4784c8f367db79fcd28b43cf59c12d5990378110
Jan <jtkuliga@gmail.com>
2021-12-04 15:27:31 +0100

    add cycle_completed and cycle_duty_reg signals in PwmControl block, add set_duty_cycle() process

--- pwm.py
+++ pwm.py
@@ -12,22 +12,37 @@ def PwmControl(clk, rst_n, duty_cycle, period, pwm):
     period - period of a time between continuous pwm cycles (in number of clock cycles)
     pwm - output
     """
-    cnt = Signal(intbv(1, 0, MAX_PWM_PERIOD))
+    cnt = Signal(intbv(0, 0, MAX_PWM_PERIOD))
+    duty_cycle_reg = Signal(intbv(0))
+    cycle_completed = Signal(bool(0))
 
     @always_seq(clk.posedge, reset=rst_n)
     def set_pwm():
         if rst_n == 0:
             pwm.next = 0
             cnt.next = 0
+            cycle_completed.next = 1
         else:
             if cnt < period:
                 cnt.next = cnt + 1
-                if cnt < duty_cycle:
+                cycle_completed.next = 0
+                if cnt < duty_cycle_reg:
                     pwm.next = 1
                 else:
                     pwm.next = 0
             else:
                 cnt.next = 0
-                pwm.next = 1
+                pwm.next = 0
+                cycle_completed.next = 1
 
-    return set_pwm
+    @always_seq(clk.posedge, reset=rst_n)
+    def set_duty_cycle():
+        if rst_n == 0:
+            duty_cycle_reg.next = 0
+        else:
+            if cycle_completed == 1:
+                duty_cycle_reg.next = duty_cycle
+            else:
+                duty_cycle_reg.next = duty_cycle_reg
+
+    return set_pwm, set_duty_cycle
--- pwm_tb.py
+++ pwm_tb.py
@@ -6,6 +6,11 @@ MAX_PWM_PERIOD = 2 ** 32 - 1
 
 @block
 def PwmControl_tb():
+    """ Test bench for the PwmControl block.
+    There are two instances of this block. They share clk, rst_n and period signals.
+    In order to check correctness of block's behavior, value of duty_cycle 
+    parameter is changed over the time.
+    """
     clk = Signal(bool(0))
     rst_n = ResetSignal(0, active=0, isasync=True)
     period = Signal(intbv(100))
@@ -30,7 +35,7 @@ def PwmControl_tb():
     def randomize_duty_cycle():
         yield delay(10)
         while True:
-            randomized_duty_cycle.next = random.randrange(period / 2)
+            randomized_duty_cycle.next = random.randrange(period)
             yield delay(2 * 100)
 
     return clk_signal, drive_reset, randomize_duty_cycle, PwmControl_inst0, PwmControl_inst1
